0abbe1f0c3b9871f1d9552798fffa417c18fd1e3
Marco Antônio <marcodsl@tuta.io>
2023-02-06 16:50:35 -0300

    feat: add cli-markdown to npm packages

--- fish/functions/upgrade.fish
+++ fish/functions/upgrade.fish
@@ -1,7 +1,7 @@
 function upgrade --description 'Gets OS X Software Updates and update Homebrew and npm packages'
     sudo softwareupdate -i -a
     brew update
-    brew upgrade --force --all
+    brew upgrade
     brew cleanup
     npm install npm -g
     npm update -g
